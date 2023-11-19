8f7f0a55ec138bfa99b53f560d5e7d3843e6cf16
Bartek <kierunb@gmail.com>
2023-11-19 20:53:33 +0100

    working-net6

---
+++ SimpleWebApplication.UnitTests/Services/AdvancedMathServiceTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+using SimpleWebApplication.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleWebApplication.Services.Tests
+{
+    public class AdvancedMathServiceTests
+    {
+        [Fact()]
+        public void VerifyInputTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+    }
+}
