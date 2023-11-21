fe2fd1a29be0e9be69005200d6aa292232d5d315
Bartek <kierunb@gmail.com>
2023-11-21 20:44:01 +0100

    updates

--- SimpleWebApplication.PlaywrightTests/SimpleTests.cs
+++ SimpleWebApplication.PlaywrightTests/SimpleTests.cs
@@ -1,4 +1,4 @@
-using Microsoft.Playwright;
+﻿using Microsoft.Playwright;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -14,6 +14,37 @@ namespace SimpleWebApplication.PlaywrightTests
             var page = await browser.NewPageAsync();
             await page.GotoAsync("https://playwright.dev/dotnet");
             await page.ScreenshotAsync(new PageScreenshotOptions { Path = "screenshot.png" });
+        }
+
+        [Fact]
+        public async Task Test2()
+        {
+            
+          
+            using var playwright = await Playwright.CreateAsync();
+            await using var browser = await playwright.Chromium.LaunchAsync();
+            var page = await browser.NewPageAsync();
+
+            await page.GotoAsync("https://www.pkobp.pl/");
+
+
+            await page.GetByLabel("Przycisk Zaakceptuj zgodę na wykorzystywanie plików cookies").ClickAsync();
+
+            await page.GetByLabel("Wpisz wartość dla pola Pole kwota pożyczki").ClickAsync();
+
+            await page.GetByLabel("Wpisz wartość dla pola Pole kwota pożyczki").ClickAsync();
+
+            await page.GetByLabel("Wpisz wartość dla pola Pole kwota pożyczki").PressAsync("ArrowRight");
+
+            await page.GetByLabel("Wpisz wartość dla pola Pole kwota pożyczki").PressAsync("ArrowRight");
+
+            await page.GetByLabel("Wpisz wartość dla pola Pole kwota pożyczki").PressAsync("ArrowRight");
+
+            await page.GetByLabel("Wpisz wartość dla pola Pole kwota pożyczki").FillAsync("5 0000");
+
+            await page.GetByLabel("Wpisz wartość dla suwaka Pole ilość rat").ClickAsync();
+
+
         }
     }
 }   
